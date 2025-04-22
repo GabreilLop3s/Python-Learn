c448ab25aca4030e44eaf736531a63825d01bcad
Gabriel <gabriellbarbosa015@gmail.com>
2025-04-22 11:29:48 -0300

    Função Lambda em  Python

---
+++ OneDrive/Desktop/ADS/python/FULL STACK/lambda.py
@@ -0,0 +1,18 @@
+"""função de potencia de um numero"""
+
+power = lambda num: num ** 2
+print(power(5))
+print(power(78))
+
+
+#pra saber se é par 
+se_par =  lambda x: x % 2 == 0
+
+#funcao que divide um numero por outro 
+divid = lambda x, y : x / y 
+
+
+
+
+
+
