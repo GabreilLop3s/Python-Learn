f658f3fcd2c815bda5d45cfbc9c078a2de45a1cb
Gabriel <gabriellbarbosa015@gmail.com>
2025-04-28 15:39:47 -0300

    primeiro commit

---
+++ 1-bd.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+#criando banco de dados
+
+conexao = sqlite3.connect('Titulo.db')
---
+++ tabela.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+conexao =  sqlite3.connect('Titulo.db')
+
+#criando cursor
+cursor = conexao.cursor()
+
+# criando a tabela
+cursor.execute(
+    """
+        CREATE TABLE filmes (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            ano INTEGER NOT NULL,
+            nota REAL NOT NULL
+        
+        );
+    """
+)
+
+#fecha a conexao
+conexao.close()
+print("Tabela foi criada")
