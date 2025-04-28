022eeea88dcdb27132b18a23aa5f05b79e10e6d2
Gabriel <gabriellbarbosa015@gmail.com>
2025-04-28 19:12:31 -0300

    corrigndo uma plavra

--- dados.py
+++ dados.py
@@ -3,7 +3,7 @@ import sqlite3
 conexao = sqlite3.connect('Titulo.db')
 cursor = conexao.cursor()
 
-cursor.execute( #cursor age como uma ponte entre o banco de dados e a IDE, como se fosse ligasse pra alguem o snal passa por algo antes de ser direcionado pra pessoa que deseja ligar
+cursor.execute( #cursor age como uma ponte entre o banco de dados e a IDE, como se você ligasse pra alguem o snal passa por algo antes de ser direcionado pra pessoa que deseja ligar
     """
             INSERT INTO filmes(nome, ano, nota)
             VALUES('Sonic',2020, 8.0)
